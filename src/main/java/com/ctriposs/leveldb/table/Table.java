5512a2fa5d9444eaab171aed836ae3399c01fbd5
yjfei <yjfei@ctrip.com>
2014-10-22 19:23:20 +0800

    no message

---
+++ src/main/java/com/ctriposs/leveldb/table/Table.java
@@ -0,0 +1,7 @@
+package com.ctriposs.leveldb.table;
+
+import com.ctriposs.leveldb.ISeekIterator;
+
+public abstract class Table implements ISeekIterator<Slice, Slice>{
+
+}
--- src/main/java/com/ctriposs/leveldb/table/TableCache.java
+++ src/main/java/com/ctriposs/leveldb/table/TableCache.java
@@ -1,5 +1,25 @@
 package com.ctriposs.leveldb.table;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.channels.FileChannel;
+
+import com.ctriposs.leveldb.storage.FileName;
+import com.google.common.cache.LoadingCache;
+
 public class TableCache {
 
+	private final LoadingCache<Long,TableFile> cache;
+	
+	
+	private static final class TableFile{
+		private final Table table;
+		
+		private TableFile(File databaseDir,long fileNumber){
+			File file = new File(databaseDir,FileName.tableFileName(fileNumber));
+			FileChannel channel = new FileInputStream(file).getChannel();
+			
+		}
+	}
+	
 }
