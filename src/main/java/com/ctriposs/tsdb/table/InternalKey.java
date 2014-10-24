2c699312595a013921db28c9d57bbfa39d1965dc
TonyAdo <coolhzb@163.com>
2014-10-24 17:24:25 +0800

    PureFileStorage fix put issue....

--- src/main/java/com/ctriposs/tsdb/table/InternalKey.java
+++ src/main/java/com/ctriposs/tsdb/table/InternalKey.java
@@ -4,13 +4,14 @@ import java.util.Comparator;
 
 import com.ctriposs.tsdb.util.ByteUtil;
 
-public class InternalKey implements Comparator<InternalKey>{
+public class InternalKey implements Comparator<InternalKey> {
+
 	private byte[] key;
+
 	public InternalKey(byte[] key){
 		this.key = key;
 	}
-	
-	
+
 	@Override
 	public int compare(InternalKey o1, InternalKey o2) {
 		return ByteUtil.compare(o1.getUserKey(), o2.getUserKey());
