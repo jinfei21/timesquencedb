afff963101762eb859e40bb9e096b82d724d32c8
TonyAdo <coolhzb@163.com>
2014-10-27 11:29:10 +0800

    FileMeta update....

--- src/main/java/com/ctriposs/tsdb/storage/FileMeta.java
+++ src/main/java/com/ctriposs/tsdb/storage/FileMeta.java
@@ -1,19 +1,51 @@
 package com.ctriposs.tsdb.storage;
 
 
+import com.ctriposs.tsdb.InternalKey;
+
 public class FileMeta {
 
-	public FileMeta(){
+    private final String fileName;
 
+    private final long fileSize;
+
+    private final InternalKey smallest;
+
+    private final InternalKey largest;
+
+	public FileMeta(String fileName, long fileSize, InternalKey smallest, InternalKey largest) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+        this.smallest = smallest;
+        this.largest = largest;
 	}
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public InternalKey getSmallest() {
+        return smallest;
+    }
+
+    public InternalKey getLargest() {
+        return largest;
+    }
 
 	@Override
 	public String toString(){
 		final StringBuilder sb = new StringBuilder();
 		sb.append("FileMeta");
-		sb.append("{number=").append("");
-
+		sb.append("{name=").append(fileName);
+        sb.append(", fileSize=").append(fileSize);
+        sb.append(", smallest=").append(smallest);
+        sb.append(", largest=").append(largest);
 		sb.append('}');
+
 		return sb.toString();
 	}
 	
