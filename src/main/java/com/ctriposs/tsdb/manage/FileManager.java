afe22bdf956a395d43a794852d939debf82c49f9
TonyAdo <coolhzb@163.com>
2014-10-28 16:32:10 +0800

    FileManager add getValue simple logical....

--- src/main/java/com/ctriposs/tsdb/manage/FileManager.java
+++ src/main/java/com/ctriposs/tsdb/manage/FileManager.java
@@ -13,10 +13,12 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import com.ctriposs.tsdb.IStorage;
 import com.ctriposs.tsdb.InternalKey;
+import com.ctriposs.tsdb.storage.DataMeta;
 import com.ctriposs.tsdb.storage.FileMeta;
 import com.ctriposs.tsdb.storage.PureFileStorage;
 import com.ctriposs.tsdb.table.InternalKeyComparator;
 import com.ctriposs.tsdb.table.MemTable;
+import com.ctriposs.tsdb.util.ByteUtil;
 import com.ctriposs.tsdb.util.FileUtil;
 
 public class FileManager {
@@ -83,11 +85,23 @@ public class FileManager {
 			for(FileMeta meta : list){
 				if(meta.contains(key)){
 					IStorage storage = new PureFileStorage(meta.getFile(), meta.getFile().length());
-						
-
+                    byte[] bytes = new byte[4];
+                    storage.get(0, bytes);
+                    int metaSize = ByteUtil.ToInt(bytes);
+                    bytes = new byte[DataMeta.META_SIZE];
+                    for (int i = 0; i < metaSize; i++) {
+                        int metaOffset = 4 + DataMeta.META_SIZE * i;
+                        storage.get(metaOffset, bytes);
+                        long code = ByteUtil.ToLong(bytes, 0);
+                        if (code == key.getCode()) {
+                            int valueSize = ByteUtil.ToInt(bytes, DataMeta.VALUE_SIZE_OFFSET);
+                            int valueOffset = ByteUtil.ToInt(bytes, DataMeta.VALUE_OFFSET_OFFSET);
+                        }
+                    }
 				}
 			}
 		}
+
 		return null;
 	}
 	
