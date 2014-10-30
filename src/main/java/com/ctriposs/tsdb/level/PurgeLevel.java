6fd4788500c44bae807989714f07bb44d9e7f7a9
yjfei <yjfei@ctrip.com>
2014-10-30 11:55:46 +0800

    fix purgelevel for filemeta

--- src/main/java/com/ctriposs/tsdb/level/PurgeLevel.java
+++ src/main/java/com/ctriposs/tsdb/level/PurgeLevel.java
@@ -1,9 +1,9 @@
 package com.ctriposs.tsdb.level;
 
-import java.io.IOException;
 import java.util.Comparator;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicLong;
 import com.ctriposs.tsdb.IStorage;
 import com.ctriposs.tsdb.InternalKey;
 import com.ctriposs.tsdb.iterator.FileSeekIterator;
@@ -16,83 +16,116 @@ public class PurgeLevel extends Level implements Runnable {
 
 	public final static long MAX_PERIOD = 1000 * 60 * 60 * 24 * 30L;
 
-    private ConcurrentSkipListMap<InternalKey, byte[]> dataListMap = new ConcurrentSkipListMap<InternalKey, byte[]>(new Comparator<InternalKey>() {
-        @Override
-        public int compare(InternalKey o1, InternalKey o2) {
-            return o1.compareTo(o2);
-        }
-    });
+	private AtomicLong purgeCounter = new AtomicLong(0);
+	private AtomicLong purgeErrorCounter = new AtomicLong(0);
+
+	private ConcurrentSkipListMap<InternalKey, byte[]> dataListMap = new ConcurrentSkipListMap<InternalKey, byte[]>(
+			new Comparator<InternalKey>() {
+				@Override
+				public int compare(InternalKey o1, InternalKey o2) {
+					return o1.compareTo(o2);
+				}
+			});
 
 	public PurgeLevel(FileManager fileManager) {
-        super(fileManager);
+		super(fileManager);
 	}
 
-	public void start(){
-		if(!run){
+	public void start() {
+		if (!run) {
 			run = true;
 			new Thread(this).start();
 		}
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		run = false;
 	}
 
 	@Override
 	public void run() {
 
-		while (run) try {
-            long start = (System.currentTimeMillis() - MAX_PERIOD) / MemTable.MINUTE * MemTable.MINUTE;
-            long end = System.currentTimeMillis() / MemTable.MINUTE * MemTable.MINUTE;
+		while (run) {
+			try {
+				long start = (System.currentTimeMillis() - MAX_PERIOD)
+						/ MemTable.MINUTE * MemTable.MINUTE;
+				long end = System.currentTimeMillis() / MemTable.MINUTE
+						* MemTable.MINUTE;
 
-            // Delete too old files
-            fileManager.delete(start);
-            for (long l = start; l < end; l++) {
-                Queue<FileMeta> fileMetaQueue = fileManager.getFiles(l);
-                if (fileMetaQueue != null && fileMetaQueue.size() >= 2) {
-                    // Just merge the beginning two meta files
-                    FileMeta metaOne = fileMetaQueue.poll();
-                    FileMeta metaTwo = fileMetaQueue.poll();
+				// Delete too old files
+				fileManager.delete(start);
+				for (long l = start; l < end; l+=MemTable.MINUTE) {
+					Queue<FileMeta> fileMetaQueue = fileManager.getFiles(l);
+					if (fileMetaQueue != null && fileMetaQueue.size() >= 2) {
+						// Just merge the beginning two meta files
+						FileMeta metaOne = fileMetaQueue.poll();
+						FileMeta metaTwo = fileMetaQueue.poll();
 
-                    String firstFileName = metaOne.getFile().getName();
-                    String secondFileName = metaTwo.getFile().getName();
-                    long numberOne = Long.valueOf(firstFileName.split("-")[1]);
-                    long numberTwo = Long.valueOf(secondFileName.split("-")[2]);
-                    IStorage iStorageOne;
-                    IStorage iStorageTwo;
+						String firstFileName = metaOne.getFile().getName();
+						String secondFileName = metaTwo.getFile().getName();
+						long numberOne = Long
+								.valueOf(firstFileName.split("-")[1]);
+						long numberTwo = Long
+								.valueOf(secondFileName.split("-")[2]);
+						IStorage iStorageOne;
+						IStorage iStorageTwo;
 
-                    if (numberOne < numberTwo) {
-                        iStorageOne = new PureFileStorage(metaOne.getFile(), metaOne.getFile().length());
-                        iStorageTwo = new PureFileStorage(metaTwo.getFile(), metaTwo.getFile().length());
-                    } else {
-                        iStorageOne = new PureFileStorage(metaTwo.getFile(), metaTwo.getFile().length());
-                        iStorageTwo = new PureFileStorage(metaOne.getFile(), metaOne.getFile().length());
-                    }
+						if (numberOne < numberTwo) {
+							iStorageOne = new PureFileStorage(
+									metaOne.getFile(), metaOne.getFile()
+											.length());
+							iStorageTwo = new PureFileStorage(
+									metaTwo.getFile(), metaTwo.getFile()
+											.length());
+						} else {
+							iStorageOne = new PureFileStorage(
+									metaTwo.getFile(), metaTwo.getFile()
+											.length());
+							iStorageTwo = new PureFileStorage(
+									metaOne.getFile(), metaOne.getFile()
+											.length());
+						}
 
-                    FileSeekIterator iteratorOne = new FileSeekIterator(iStorageOne);
-                    FileSeekIterator iteratorTwo = new FileSeekIterator(iStorageTwo);
+						FileSeekIterator iteratorOne = new FileSeekIterator(
+								iStorageOne);
+						FileSeekIterator iteratorTwo = new FileSeekIterator(
+								iStorageTwo);
 
-                    iteratorOne.seekToFirst();
-                    iteratorTwo.seekToFirst();
+						iteratorOne.seekToFirst();
+						iteratorTwo.seekToFirst();
 
-                    while (iteratorOne.hasNext()) {
-                        dataListMap.put(iteratorOne.key(), iteratorOne.value());
-                        iteratorOne.next();
-                    }
+						while (iteratorOne.hasNext()) {
+							dataListMap.put(iteratorOne.key(),
+									iteratorOne.value());
+							iteratorOne.next();
+						}
 
-                    while (iteratorTwo.hasNext()) {
-                        dataListMap.put(iteratorTwo.key(), iteratorTwo.value());
-                        iteratorTwo.next();
-                    }
+						while (iteratorTwo.hasNext()) {
+							dataListMap.put(iteratorTwo.key(),
+									iteratorTwo.value());
+							iteratorTwo.next();
+						}
 
-                    // Write to file
-                    storeFile(l, dataListMap, fileManager.getFileNumber());
-                    dataListMap.clear();
-                }
-            }
-        } catch (IOException e) {
-
-        }
-		
+						// Write to file
+						FileMeta fileMeta = storeFile(l, dataListMap, fileManager.getFileNumber());
+						dataListMap.clear();
+						
+						
+						fileManager.add(l, fileMeta);
+						
+					}
+				}
+				purgeCounter.incrementAndGet();
+			} catch (Throwable e) {
+				e.printStackTrace();
+				purgeErrorCounter.incrementAndGet();
+			}finally{
+				try {
+					Thread.sleep(5*MemTable.MINUTE);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
