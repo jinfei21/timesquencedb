62b336c6b07c358adb18a6dde3ccc3330f0f13d5
TonyAdo <coolhzb@163.com>
2014-11-11 14:15:25 +0800

    DBEngineCompactFunctionTest update....

--- src/test/java/com/ctriposs/tsdb/test/DBEngineCompactFunctionTest.java
+++ src/test/java/com/ctriposs/tsdb/test/DBEngineCompactFunctionTest.java
@@ -2,6 +2,7 @@ package com.ctriposs.tsdb.test;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Random;
 
 import com.ctriposs.tsdb.DBConfig;
 import com.ctriposs.tsdb.DBEngine;
@@ -9,6 +10,8 @@ import com.ctriposs.tsdb.DBEngine;
 public class DBEngineCompactFunctionTest {
 
     private static final String TEST_DIR = "d://tsdb_test/compact/";
+    private static final String[] TABLE_NAME_ARRAY = new String[]{"a", "b", "c", "d", "e", "f", "g"};
+    private static final String[] COLUMN_NAME_ARRAY = new String[]{"A", "B", "C", "D", "E", "F", "G"};
 
     private static DBEngine engine;
 
@@ -16,10 +19,11 @@ public class DBEngineCompactFunctionTest {
 
         DBConfig config = new DBConfig(TEST_DIR);
         engine = new DBEngine(config);
+        Random random = new Random();
 
         System.out.println("Start from date " + System.currentTimeMillis());
         for (long counter = 0;; counter++) {
-            engine.put("a", "b", System.currentTimeMillis(), "fdsafasdfasdfasdfsdafsdafasdfasdfasdfasfsda".getBytes());
+            engine.put(TABLE_NAME_ARRAY[random.nextInt(TABLE_NAME_ARRAY.length)], COLUMN_NAME_ARRAY[random.nextInt(COLUMN_NAME_ARRAY.length)], System.currentTimeMillis(), "fdsafasdfasdfasdfsdafsdafasdfasdfasdfasfsda".getBytes());
 
             if (counter  % 1000000 == 0) {
                 System.out.println("Current date:" + new Date());
